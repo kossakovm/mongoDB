2bd047b00a7ec94a53aac319c23f455c81372bca
Marlen <marlenk69@gmail.com>
2022-08-03 12:35:03 +0600

    first commit

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #/!bin/bash
 mongorestore -d reservations reservations/
-mongod --replSet rs0 --port 27017 --bind_ip localhost,mongo-1 --dbpath /srv/mongodb/rs0-0  --oplogSize 128
-mongod --replSet rs0 --port 27018 --bind_ip localhost,mongo-2 --dbpath /srv/mongodb/rs0-1  --oplogSize 128
-mongod --replSet rs0 --port 27019 --bind_ip localhost,mongo-3 --dbpath /srv/mongodb/rs0-2 --oplogSize 128
+
+config = {_id:"mongo-set",members:[{_id:0,host:"mongo-1:27017"},{_id:1,host:"mongo-2:27017"},{_id:2,host:"mongo-3:27017"}]};
+rs.initiate(config);
+
+rs.slaveOk()
